35e5d1f2efa3dd7bb2601f5f63b290a5616aa2eb
Neev Grover <groverneev@gmail.com>
2025-06-01 19:42:32 -0700

    Add navigation bar to all pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -59,7 +59,7 @@ export default function AboutPage() {
         borderRadius: 12,
         textAlign: "center"
       }}>
-        <strong>Want to learn more or get involved?</strong>
+        <strong> color: "#000000", Want to learn more or get involved?</strong> {/* this was changed */}
         <br />
         <a href="/contact" style={{
           color: "#000000",
