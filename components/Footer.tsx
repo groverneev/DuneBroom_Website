ee2d009cc60acd537ea480040ecf0466ad75fb37
Neev Grover <groverneev@gmail.com>
2025-06-02 15:33:22 -0700

    fix_footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,22 +10,6 @@ export default function Footer() {
     }}>
       &copy; {new Date().getFullYear()} DuneBroom. All rights reserved.
       <br />
-      <a 
-        href="/contact" 
-        style={{
-          display: 'inline-block',
-          marginTop: 12,
-          padding: '8px 20px',
-          background: '#bbf7d0',
-          color: '#166534',
-          borderRadius: 8,
-          textDecoration: 'none',
-          fontWeight: 600,
-          transition: 'background 0.2s'
-        }}
-      >
-        Contact
-      </a>
     </footer>
   );
 }
