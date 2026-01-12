e0d46ddf0e75b86b191219b1293fc240ef558454
Neev Grover <groverneev@gmail.com>
2026-01-12 15:47:39 -0800

    minor edits

--- app/about_us/page.tsx
+++ app/about_us/page.tsx
@@ -129,7 +129,6 @@ export default function AboutPage() {
                 }}
               />
             </div>
-            
             <h1
               style={{
                 fontSize: 36,
